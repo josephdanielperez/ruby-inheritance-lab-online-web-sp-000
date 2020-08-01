f4589c89d733ae62276adcc757a4025821b27907
Joseph Perez <hello@josephdanielperez.com>
2020-08-01 21:03:32 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,12 +1,6 @@
 class Teacher < User
-
-  attr_accessor :knowledge
+  
   @@KNOWLEDGE = ["a String is a type of data in Ruby", "programming is hard, but it's worth it", "javascript async web request", "Ruby method call definition", "object oriented dog cat class instance", "class method class variable instance method instance variable", "programming computers hacking learning terminal", "bash Ruby rvm update certs"]
-
-  def initialize
-    @first_name = first
-    @last_name = last
-  end
 
   def teach
     @@KNOWLEDGE[rand(0..7)]
